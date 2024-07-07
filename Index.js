89e3d4dee690701bdffa5296382fbdad969b95e6
AnyiCMZ123 <angelabelencastillomartinez@gmail.com>
2024-07-07 15:37:03 -0600

    Add new line in response

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ let http = require("http");
 
 let handler = function(request, response){
     console.log("we receive a new request");
-    response.end("Hello World");
+    response.end("Hello World \n :)");
 };
 
 let servidor = http.createServer(handler);
