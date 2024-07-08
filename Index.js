89ae478e29402a018a1fa69f4d7baa057bd14cfb
AnyiCMZ123 <angelabelencastillomartinez@gmail.com>
2024-07-08 11:40:11 -0600

    HTML code

--- Index.js
+++ Index.js
@@ -1,8 +1,22 @@
 let http = require("http");
 
 let handler = function(request, response){
-    console.log("we receive a new request");
-    response.end("Hello World \n :)");
+    console.log('we receive a new request');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Hello World</title>
+            <style>
+                .blue { color: blue; }
+            </style>
+        </head>
+        <body>
+            Hello World<br><span class="blue">:)</span>
+        </body>
+        </html>
+    `);
 };
 
 let servidor = http.createServer(handler);
